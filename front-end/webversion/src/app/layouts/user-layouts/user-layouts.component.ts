2773be8a182a5a9498d23087a3cde314d8cc2a13
amalromdhane <romdhane.996@gmail.com>
2024-10-24 11:37:42 +0200

    landing page

---
+++ front-end/webversion/src/app/layouts/user-layouts/user-layouts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-layouts',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-layouts.component.html',
+  styleUrl: './user-layouts.component.css'
+})
+export class UserLayoutsComponent {
+
+}
