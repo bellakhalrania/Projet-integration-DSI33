d2dd2513390498fbbd83f46692b15d7510849386
Rania <bellakhalrania12@gmail.com>
2024-10-24 10:42:37 +0100

    commit

--- front-end/webversion/src/app/app.routes.ts
+++ front-end/webversion/src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
-import { UserLayoutsComponent } from './layouts/user-layouts/user-layouts.component';
-import { AdminLayoutsComponent } from './layouts/admin-layouts/admin-layouts.component';
-import { HomeComponent } from './layouts/user-layouts/home/home.component';
+
 
 
 export const routes: Routes = [
-    { path: "", component: HomeComponent },  
-    { path: "user-layout", component: UserLayoutsComponent },   
-    { path: "admin-layout", component: AdminLayoutsComponent}, 
+   
 
 
     
