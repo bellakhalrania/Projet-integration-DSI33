0f44274e0b9187f116b22f893fd1ddb579212383
Rania <bellakhalrania12@gmail.com>
2024-10-24 14:39:49 +0100

    new change

--- front-end/webversion/src/app/app.component.ts
+++ front-end/webversion/src/app/app.component.ts
@@ -3,15 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { LayoutsModule } from './layouts/layouts.module';
 import { AdminFrontComponent } from './layouts/admin-front/admin-front.component';
 import { UserfrontComponent } from './layouts/userfront/userfront.component';
-import { TodolisteComponent } from './layouts/views/user/todoliste/todoliste.component';
+import { SigninComponentComponent } from './layouts/signin-component/signin-component.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LayoutsModule,AdminFrontComponent,
-    UserfrontComponent,TodolisteComponent],
+  imports: [RouterOutlet,AdminFrontComponent,
+    UserfrontComponent,SigninComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front-end/webversion/src/app/app.routes.ts
+++ front-end/webversion/src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import { AdminFrontComponent } from './layouts/admin-front/admin-front.component';
 import { UserfrontComponent } from './layouts/userfront/userfront.component';
+import { SigninComponentComponent } from './layouts/signin-component/signin-component.component';
 
 
 
 
 export const routes: Routes = [
-    {path:'',component:UserfrontComponent,children:[
-        {path:'to',loadComponent:()=>import('./layouts/views/user/todoliste/todoliste.component').then(c=>c.TodolisteComponent)}
-       
-    ]}
-    
-    ,{path:'admin',component:AdminFrontComponent}
+    {path:'',component:UserfrontComponent},
+
+    {path:'admin',component:AdminFrontComponent}
+    ,
+    {path:'signin',component:SigninComponentComponent}
 ];
