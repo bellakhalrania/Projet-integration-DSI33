360383036fa1dba8f214c2a28e5cfea90e1b9724
amalromdhane <romdhane.996@gmail.com>
2024-10-30 13:17:15 +0100

    new changes

--- front-end/webversion/src/app/app.component.ts
+++ front-end/webversion/src/app/app.component.ts
@@ -6,19 +6,14 @@ import { UserfrontComponent } from './layouts/userfront/userfront.component';
 import { SigninComponentComponent } from './layouts/signin-component/signin-component.component';
 import { HomeComponent } from './views/front/home/home.component';
 import { AboutComponent } from './views/front/about/about.component';
-import { RecourcesComponent } from './views/front/recources/recources.component';
-import { WorkShopComponent } from './views/front/work-shop/work-shop.component';
-import { RelaxComponent } from './views/front/relax/relax.component';
-import { ArticlesComponent } from './views/front/articles/articles.component';
-import { InitiativesComponent } from './views/front/initiatives/initiatives.component';
-import { GroupsComponent } from './views/front/groups/groups.component';
-import { QuizsComponent } from './views/front/quizs/quizs.component';
+
+
 
 
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 import { SignupComponentComponent } from './layouts/signup-component/signup-component.component';
-import { TodolistComponent } from './layouts/userfront/todo/todolist/todolist.component';
+
 
 
 
@@ -29,9 +24,7 @@ import { TodolistComponent } from './layouts/userfront/todo/todolist/todolist.co
 
   imports: [RouterOutlet,AdminFrontComponent,
     UserfrontComponent,HomeComponent,SigninComponentComponent,SignupComponentComponent,
-    AboutComponent,TodolistComponent,
-    RecourcesComponent,WorkShopComponent,RelaxComponent,ArticlesComponent,InitiativesComponent,
-    GroupsComponent,QuizsComponent,ReactiveFormsModule],
+    AboutComponent,ReactiveFormsModule],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- front-end/webversion/src/app/app.routes.ts
+++ front-end/webversion/src/app/app.routes.ts
@@ -7,13 +7,9 @@ import { UserfrontComponent } from './layouts/userfront/userfront.component';
 import { HomeComponent } from './views/front/home/home.component';
 import { AboutComponent } from './views/front/about/about.component';
 import { EventsComponent } from './views/front/events/events.component';
-import { ArticlesComponent } from './views/front/articles/articles.component';
+
 import { ContactComponent } from './views/front/contact/contact.component';
-import { GroupsComponent } from './views/front/groups/groups.component';
-import { InitiativesComponent } from './views/front/initiatives/initiatives.component';
-import { QuizsComponent } from './views/front/quizs/quizs.component';
-import { RelaxComponent } from './views/front/relax/relax.component';
-import { WorkShopComponent } from './views/front/work-shop/work-shop.component';
+
 
 import { SigninComponentComponent } from './layouts/signin-component/signin-component.component';
 import { SignupComponentComponent } from './layouts/signup-component/signup-component.component';
@@ -27,17 +23,10 @@ import { SignupComponentComponent } from './layouts/signup-component/signup-comp
 
 export const routes: Routes = [
     { path:'',component:UserfrontComponent,children: [
-    {path:'home',loadComponent:()=>import('./views/front/home/home.component').then(c=>c.HomeComponent)},
-    {path:'todoliste',loadComponent:()=>import('./layouts/userfront/todo/todolist/todolist.component').then(c=>c.TodolistComponent)},
+
     {path:'about',loadComponent:()=>import('./views/front/about/about.component').then(c=>c.AboutComponent)},
     {path:'events',loadComponent:()=>import('./views/front/events/events.component').then(c=>c.EventsComponent)},
-    {path:'workShop',loadComponent:()=>import('./views/front/work-shop/work-shop.component').then(c=>c.WorkShopComponent)},
-    {path:'groups',loadComponent:()=>import('./views/front/groups/groups.component').then(c=>c.GroupsComponent)},
-    {path:'quizs',loadComponent:()=>import('./views/front/quizs/quizs.component').then(c=>c.QuizsComponent)},
-    {path:'relax',loadComponent:()=>import('./views/front/relax/relax.component').then(c=>c.RelaxComponent)},
-    {path:'resources',loadComponent:()=>import('./views/front/recources/recources.component').then(c=>c.RecourcesComponent)},
-    {path:'articles',loadComponent:()=>import('./views/front/articles/articles.component').then(c=>c.ArticlesComponent)},
-    {path:'initiatives',loadComponent:()=>import('./views/front/initiatives/initiatives.component').then(c=>c.InitiativesComponent)},
+
     {path:'contact',loadComponent:()=>import('./views/front/contact/contact.component').then(c=>c.ContactComponent)},
      ]}
 
