bdf7819fbef281f6d77106c680e0a9a8d3ae861c
Rania <bellakhalrania12@gmail.com>
2024-12-19 20:27:26 +0100

    changement  gateway

--- back-end/gateway-service/src/main/java/org/id/GatewayServiceApplication.java
+++ back-end/gateway-service/src/main/java/org/id/GatewayServiceApplication.java
@@ -15,6 +15,20 @@ public class GatewayServiceApplication {
 	@Bean
 	RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
 	return builder.routes()
+
+
+             .route(r->r.path("/api/exercices/**").uri("http://localhost:8081/"))
+
+             .route(r->r.path("/api/groups/**").uri("http://localhost:8084/"))
+
+        
+			 // Route for CUSTOMER-SERVICE
+            .route(r -> r.path("/customers/**")
+                    .uri("lb://CUSTOMER-SERVICE")) // Load-balanced URI for the customer service
+            // Route for INVENTORY-SERVICE
+            .route(r -> r.path("/products/**")
+                    .uri("lb://INVENTORY-SERVICE")) // Load-balanced URI for the inventory service
+            // Route for Express application
             .route("express_route", r -> r.path("/api/**")
                     .uri("http://localhost:3000")) // Direct URI for the Express app
             .build();
